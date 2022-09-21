cfe85d6f6f96027f4cbc861699011a2e7071e866
Mr-Basirnia <mr.basirnia@gmail.com>
2022-09-21 20:19:53 +0430

    add single page post author

--- templates/blog/single/content.php
+++ templates/blog/single/content.php
@@ -29,3 +29,31 @@
     </div>
 </section>
 <hr>
+
+<?php
+/*-------------------------------------
+* Get Post Author
+*-----------------------------------*/
+?>
+<div class="component-section">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="row">
+                    <div class="col-md-3 text-md-center">
+                        <img class="avatar-lg rounded-circle mb-3 d-inline-block"
+                             src="<?= get_avatar_url( get_the_author_meta( 'ID' ), array ( 'size' => 240 ) ) ?>"
+                             alt="<?= get_the_author_meta( 'display_name' ) ?>">
+                    </div>
+                    <div class="col-md-9">
+                        <h5 class="font-lora font-weight-normal mb-4"><?= get_the_author_meta( 'description' ) ?></h5>
+                        <div class="border-left mb-3"> &nbsp;</div>
+                        <strong class="text-primary"><?= get_the_author_meta( 'display_name' ) ?></strong>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<hr>
