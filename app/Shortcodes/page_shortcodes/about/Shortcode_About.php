d943e6d562516e15f687e8661956d7db29ff6067
Mr-Basirnia <mr.basirnia@gmail.com>
2022-09-26 19:32:52 +0330

    ADD point shortcode for about page TODO:: This ShortCode needs dynamization

--- app/Shortcodes/page_shortcodes/about/Shortcode_About.php
+++ app/Shortcodes/page_shortcodes/about/Shortcode_About.php
@@ -14,6 +14,7 @@ class Shortcode_About {
 	 */
 	public function __construct() {
 		add_shortcode( 'clab_about_us', array ( $this, 'about_us_callback' ) );
+		add_shortcode( 'clab_point', array ( $this, 'point_callback' ) );
 	}
 
 	/**
@@ -51,5 +52,45 @@ class Shortcode_About {
         </div>';
 
 		return $o;
+	}
+
+	/**
+	 * It returns a string of point HTML code.
+	 *
+	 * @return string
+	 */
+	public function point_callback(): string {
+		$o = '
+		<div class="section-gap bg-gray">
+			<div class="container">
+				<div class="row justify-content-between align-items-center">
+					<div class="col-md-8 mb-lg-5 mb-4">
+						<h6>هدف ما</h6>
+						<h2 class="mb-4">ما در اینجا برای حل مشکل شما و تحویل نیازهای شما هستیم</h2>
+					</div>
+					<div class="col-md-6">
+						<h4>ماموریت</h4>
+						<p class="text-muted">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+						<ul class="list-unstyled text-muted">
+							<li><i class="vl-minus font-size-12 pr-3 pb-0"></i>یکی از بهترین سایت های چند منظوره</li>
+							<li><i class="vl-minus font-size-12 pr-3 pb-0"></i>پشتیبانی حرفه ای و سریع </li>
+							<li><i class="vl-minus font-size-12 pr-3 pb-0"></i>کد های مرتب و تمیز</li>
+						</ul>
+					</div>
+					<div class="col-md-6">
+						<h4>ویژن</h4>
+						<p class="text-muted">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+						<ul class="list-unstyled text-muted">
+							<li><i class="vl-minus font-size-12 pr-3 pb-0"></i>یکی از بهترین سایت های چند منظوره</li>
+							<li><i class="vl-minus font-size-12 pr-3 pb-0"></i>پشتیبانی حرفه ای و سریع </li>
+							<li><i class="vl-minus font-size-12 pr-3 pb-0"></i>کد های مرتب و تمیز</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		';
+
+		return $o;    // TODO:: This ShortCode needs dynamization.
 	}
 }
